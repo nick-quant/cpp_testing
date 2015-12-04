70013fc50bf5269fef76901e3c8a0a010c99dd9c
Nikolay Nadirashvili <n.nadirashvili@tum.de>
2015-12-04 17:19:28 +0100

    A bit of templates

--- vec.cpp
+++ vec.cpp
@@ -3,15 +3,11 @@
 
 int main()
 {
-	std::cout << "Start\n";
-	std::vector<int> vec(1000, 0);
-	
-	int exit = 0;	
-	while (!exit)
-	{
-		vec.push_back(3);
-		std::cout << vec.size() << "\t" << vec.capacity() << "\n";
-		std::cin >> exit;	
-	}
+	double a = 0.159 + 0.141;
+	float b = 0.1 + 0.2;
+	float sum = 0;
+	for (int i = 0; i < 24; ++i)
+		sum += 2 * sum + a - b;
+	std::cout << sum << std::endl;
 	return 0;
 }
